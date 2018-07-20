4823ee8386fe989fc00297ba1ff59648834e4cfe
Illya Moskvin <imoskvin@artic.edu>
2018-07-19 21:44:52 -0500

    Add `Get-MacroPath` function

--- test/test.ps1
+++ test/test.ps1
@@ -1,8 +1,23 @@
 $DirCapture = Join-Path $PSScriptRoot -ChildPath 'capture'
 
+
+function Get-MacroPath ([string]$Path) {
+
+    # Replace drive prefix, uppercase
+    @('^([A-Za-z]):\\', '%drive_([A-Za-z])%\\') | ForEach-Object {
+        if ($Path -match $_) {
+            $drive = [regex]::match($Path, $_).Groups[1].Value
+            $drive = $drive.ToUpper()
+            $Path = $Path -replace $_, "%drive_$drive%\"
+        }
+    }
+
+    $Path
+}
+
+
 function Get-CapturePath ([string]$Path) {
-    # TODO: Consider adding a transform function to match
-    Join-Path $DirCapture -ChildPath $Path
+    Join-Path $DirCapture -ChildPath (Get-MacroPath $Path)
 }
 
 
@@ -16,4 +31,5 @@ function Add-Dir ([string]$Path) {
 }
 
 
+Get-CapturePath 'x:\'
 Get-CapturePath 'X:\'
